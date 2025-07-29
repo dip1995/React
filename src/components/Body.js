6a33c99d5ebeb471617a43d6be926e0d4cff485c
dip1995 <vyas.diptesh@gmail.com>
2025-07-29 21:56:04 +0530

    add shimmer & call api and search input

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,49 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState, useEffect } from "react";
+import Shimmer from "./shimmer";
 const Body = () => {
-   
-   return (
+   const [ListOfRestaurants, setListOfRestaurants] = useState([]);
+   const [CopyListOfRestaurants, setCopyListOfRestaurants] = useState([]);
+
+   const [searchText, setSearchText] = useState("");
+
+    useEffect(() => {
+    fetchRestaurants();
+    }, []);
+
+   const fetchRestaurants = async () => {
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      const json = await data.json();
+      console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+      setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setCopyListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+   }
+  
+   return ListOfRestaurants.length === 0 ? <Shimmer /> :  (
     <div className="body-container">
-     <RestaurantCard />
+     <div className="cart-container">
+            <div className="search-container">
+               <input type="text" className="search-input" onChange={(e) => {
+                  setSearchText(e.target.value);
+               }} value={searchText} placeholder="Search for restaurants" />
+               <button className="search-btn" onClick={() => {
+                  console.log(searchText);
+                   const filteredList = ListOfRestaurants.filter((item) => item.info.name.toLowerCase().includes(searchText.toLowerCase()))
+                   setCopyListOfRestaurants(filteredList);                  
+               }}>Search</button>
+            </div>
+            <button className="btn-rating" onClick={() => {
+                const filteredList = ListOfRestaurants.filter((item) => item.info.avgRating > 4.3);
+                setListOfRestaurants(filteredList);
+            }}>Top Rating</button>
+            <div className="card-details">
+              {
+              CopyListOfRestaurants.map((restaurant) => {
+                return <RestaurantCard resData={restaurant?.info} key={restaurant?.info?.id}/>
+              })
+              }        
+           </div>
+        </div>
     </div>      
    ) 
 }
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,33 +1,21 @@
-import resList from "../resList";
-import { useState } from "react";
+import { CDN_URL } from "../utils/constant";
 
-const RestaurantCard = () => {
-    const [ListOfRestaurants, setListOfRestaurants] = useState(resList.restaurants);
-    return (
-        <div className="cart-container">
-            <button className="btn-rating" onClick={() => {
-                const filteredList = ListOfRestaurants.filter((item) => item.ratings.average > 4);
-                setListOfRestaurants(filteredList);
-            }}>Top Rating</button>
-            <div className="card-details">
-              {
-              ListOfRestaurants.map((params) => {
-                return (
-                <div key={params.id} className="card-item">
-                    <div className="card">
-                    <img className="card-img" src={params.imageUrl} alt="cart-item" />
-                    </div>
-                    <h2 className="card-title">{params.name}</h2>
-                    <p className="card-cuisine">{params.cuisines.join(", ")}</p>
-                    <p className="card-rating">Rating: {params.ratings.average}</p>
-                    <p className="card-review">Review: ₹{params.ratings.totalReviews}</p>
-                </div>
-                )
-              })
-              }        
-           </div>
-        </div>
-    )
-
-}
+const RestaurantCard = (props) => {
+  const { cloudinaryImageId, name, avgRating, cuisines, costForTwo } = props.resData;
+  return (
+    <div className="card-item">
+      <div className="card">
+        <img
+        className="card-img"
+        alt="res-logo"
+        src="https://picsum.photos/id/1062/800/600"
+      />
+      </div>
+      <h2 className="card-title">{name}</h2>
+      <p className="card-cuisine">{cuisines.join(", ")}</p>
+      <p className="card-rating">Rating: {avgRating}</p>
+      <p className="card-review">Review: ₹{costForTwo}</p>
+    </div>
+  );
+};
 export default RestaurantCard;
